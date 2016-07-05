447843ca19223ea2a761c8e8bd7d0b37e43aa5e1
wheeler36 <wheeler.hord@gmail.com>
2016-07-05 14:23:58 -0800

    Edit and destroy articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :find_article, only: [:show, :edit, :destroy]
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = current_user.articles.build(articles_params)
+    @article = current_user.articles.build(article_params)
 
     if @article.save
       redirect_to @article
@@ -28,13 +28,29 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to root_path
+  end
+
   private
 
   def find_article
     @article = Article.find(params[:id])
   end
 
-  def articles_params
+  def article_params
     params.require(:article).permit(:title, :content, :category_id)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :articles
+  has_many :articles, dependent: :destroy
 end
