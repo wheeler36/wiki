17cc242a4a69f1e3cb88281caf4c1c79cde3cf57
wheeler36 <wheeler.hord@gmail.com>
2016-07-05 13:20:07 -0800

    Add devise and generate user model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
   before_action :find_article, only: [:show, :edit, :destroy]
 
   def index
+    @articles = Article.all.order("Created_at DESC")
   end
 
   def show
