11e743ffe6a0789e7582d64a89b1f239da32af6c
wheeler36 <wheeler.hord@gmail.com>
2016-07-05 13:36:01 -0800

    Add user authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :find_article, only: [:show, :edit, :destroy]
-
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @articles = Article.all.order("Created_at DESC")
   end
